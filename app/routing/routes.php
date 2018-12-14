06249c7d6e18ed5edac760badc765e33d821daa1
StabijJack <jj.schneider@live.nl>
2018-12-14 21:01:12 +0100

    Creating our first Controller

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -1,11 +1,15 @@
 <?php
 $router = new AltoRouter;
-$router->map('GET','/about','','about_us');
+$router->map('GET','/','','home');
 
 $match = $router->match();
 
 if ($match) {
-    echo 'about page';
+    require_once __DIR__ . '/../controllers/BaseController.php';
+    require_once __DIR__ . '/../controllers/IndexController.php';
+
+    $index = new App\Controllers\IndexController();
+    $index->show();
 }
 else {
     header($_SERVER['SERVER_PROTOCOL'].'404 Not Found');
