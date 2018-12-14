eada5be37c37b049c4f6fe21af6d188bcd955e9d
StabijJack <jj.schneider@live.nl>
2018-12-14 22:59:53 +0100

    Creating a Route Dispatcher

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -1,17 +1,3 @@
 <?php
 $router = new AltoRouter;
-$router->map('GET','/','','home');
-
-$match = $router->match();
-
-if ($match) {
-    require_once __DIR__ . '/../controllers/BaseController.php';
-    require_once __DIR__ . '/../controllers/IndexController.php';
-
-    $index = new App\Controllers\IndexController();
-    $index->show();
-}
-else {
-    header($_SERVER['SERVER_PROTOCOL'].'404 Not Found');
-    echo "Page not found";
-}
+$router->map('GET','/','App\Controllers\IndexController@show','home');
