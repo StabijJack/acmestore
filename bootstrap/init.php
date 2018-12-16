e3009f19829da615c9ebd5e8d928aec104f4f62a
StabijJack <jj.schneider@live.nl>
2018-12-16 14:39:04 +0100

    Install and Setup Eloquent ORM

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -6,5 +6,6 @@ if(!isset($_SESSION)) session_start();
 
 //load environment variable
 require_once __DIR__ . '/../app/config/_env.php';
+new \App\Classes\Database();
 require_once __DIR__ . '/../app/routing/routes.php';
 new \App\RouteDispatcher($router);
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
 require_once __DIR__ . '/../bootstrap/init.php';
+$app_name = getenv('APP-Name');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+$user = Capsule::table('categories')->where('id', 1)->first();
+var_dump($user);
