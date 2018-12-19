e8954d02b21c567817f4fbade051152db182b40d
StabijJack <jj.schneider@live.nl>
2018-12-19 22:15:31 +0100

    Form Request Validation part 2

--- app/classes/ValidateRequest.php
+++ app/classes/ValidateRequest.php
@@ -16,12 +16,45 @@ class ValidateRequest
     {
         return $value != null && !empty(trim($value));
     }
-    public static function minLength($column, $value, $policy)
+    protected static function minLength($column, $value, $policy)
     {
         return strlen($value) >= $policy;
     }
-    public static function maxLength($column, $value, $policy)
+    protected static function maxLength($column, $value, $policy)
     {
         return strlen($value) <= $policy;
+    }
+    protected static function email($column, $value, $policy)
+    {
+        if ($value != null && !empty(trim($value))) {
+            return filter_var($value, FILTER_VALIDATE_EMAIL);
+        }
+    }
+    protected static function mixed($column, $value, $policy)
+    {
+        if ($value != null && !empty(trim($value))) {
+            if(!preg_match('/^[A-Za-z0-9 .,_~\-!@#&%\^\'\*\(\)]+$/', $value)){
+                return true;
+            }
+        }
+        return false;
+    }
+    protected static function string($column, $value, $policy)
+    {
+        if ($value != null && !empty(trim($value))) {
+            if(!preg_match('/^[A-Za-z ]+$/', $value)){
+                return true;
+            }
+        }
+        return false;
+    }
+    protected static function number($column, $value, $policy)
+    {
+        if ($value != null && !empty(trim($value))) {
+            if(!preg_match('/^[0-9.,]+$/', $value)){
+                return true;
+            }
+        }
+        return false;
     }
 }
