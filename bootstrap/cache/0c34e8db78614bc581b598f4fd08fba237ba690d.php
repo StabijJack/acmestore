454e583881da5fef9fdd143bcaeed983e0652123
StabijJack <jj.schneider@live.nl>
2018-12-30 20:59:33 +0100

    Styling The Front Store Navigation

--- bootstrap/cache/0c34e8db78614bc581b598f4fd08fba237ba690d.php
+++ bootstrap/cache/0c34e8db78614bc581b598f4fd08fba237ba690d.php
@@ -1,14 +1,18 @@
 <?php $categories = \App\Models\Category::with('subCategories')->get() ?>
 <header class="navigation">
-    <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle="example-menu"></button>
-        <div class="title-bar-title">Menu</div>
+    <div class="title-bar" data-responsive-toggle="main-menu" data-hide-for="medium">
+        <button class="menu-icon float-right" type="button" data-toggle="main-menu"></button>
+        <a href="/" class="logo float-left"></a>
     </div>
 
     <div class="top-bar" id="main-menu">
+        <div class="top-bar-title show-for-medium">
+            <a href="/" class="logo"></a>
+        </div>
         <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
-                <li>Acme Products</li>
+            <ul class="drilldown  menu vertical medium-horizontal" data-dropdown-menu
+                data-disable-hover="true" data-click-open="true">
+                <li><a href="#">Acme Products</a></li>
                 <?php if(count($categories)): ?>
                     <li>
                         <a href="#">Categories</a>
@@ -33,9 +37,9 @@
             </ul>
         </div>
         <div class="top-bar-right">
-            <ul class="menu">
-                <li>Username</li>
-                <li><a href='#'>Sign in</a></li>
+            <ul class="menu vertical medium-horizontal">
+                    <li><a href="#">User Name</a></li>
+                    <li><a href='#'>Sign in</a></li>
                 <li><a href='#'>Register</a></li>
                 <li><a href='#'>Cart</a></li>
             </ul>
