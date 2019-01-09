5d0cb2e7eeea98b44b0a1304228d5648ee610a9b
StabijJack <jj.schneider@live.nl>
2019-01-09 11:23:48 +0100

    Product Details Page - Part 3 (PHP & VueJs)

--- bootstrap/cache/c44162dd338d47853d36388bf1007c35926b1d37.php
+++ bootstrap/cache/c44162dd338d47853d36388bf1007c35926b1d37.php
@@ -2,26 +2,41 @@
 <?php $__env->startSection('title'); ?> <?php echo e($product->name); ?><?php $__env->stopSection(); ?>
 <?php $__env->startSection('data-page-id', 'product'); ?> 
 <?php $__env->startSection('content'); ?>
-    <div class="product" id="product" style="padding: 6rem;" data-token=<?php echo e($token); ?> data-id="<?php echo e($product->id); ?>">
+    <div class="product" id="product" data-token=<?php echo e($token); ?> data-id="<?php echo e($product->id); ?>">
         <div class="text-center">
             <i v-show="loading" class="fa fa-spinner" style="font-size: 3rem; padding-bottom: 3rem; color:#0a0a0a"> </i>
         </div>
-        <section class="item-container">
+        <section class="item-container" v-if="loading == false">
             <div class="row column">
                 <nav aria-label="You are here:" role="navigation">
                     <ul class="breadcrumbs">
-                        <li><a href="#">Product Category</a></li>
-                        <li><a href="#">Product Subcategory</a></li>
-                        <li>>Product name</li>
+                        <li>
+                            <a :href="'/product/category/' + category.slug">
+                                {{ category.name }}
+                            </a>
+                        </li>
+                        <li>
+                            <a :href="'/product/subcategory/' + subCategory.slug">
+                                {{ subCategory.name }}
+                            </a>
+                        </li>
+                        <li>>{{ product.name }}</li>
                     </ul>
                 </nav>
             </div>
             <div class="row collapse">
                 <div class="small-12 medium-5 large-4 column">
-                    
+                    <div >
+                        <img :src="'/'+ product.image_path" width="100%" height="200">
+                    </div>
                 </div>
                 <div class="small-12 medium-7 large-8 column">
-
+                    <div class="product-details">
+                        <h2>{{ product.name }}</h2>
+                        <p>{{ product.description }}</p>
+                        <h2>${{ product.price }}</h2>
+                        <button class="button alert">Add to Cart</button>
+                    </div>
                 </div>
             </div>
         </section>
--- resources/assets/js/pages/product_details.js
+++ resources/assets/js/pages/product_details.js
@@ -6,9 +6,38 @@
             data:{
                 product: [],
                 category:[],
-                subCatagory: [],
+                subCategory: [],
                 productId: $('#product').data('id'),
                 loading: false
+            },
+            methods:{
+                getProductDetails: function (){
+                    this.loading = true;
+                    setTimeout(
+                        function(){
+                            axios.get('/product-details/' + this.productId
+                            ).then(
+                                function (response) {
+                                    app.product = response.data.product;
+                                    app.category = response.data.category;
+                                    app.subCategory = response.data.subCategory;
+                                    app.loading = false;
+                                }
+                            );
+                        }, 
+                        1000
+                    );
+                },
+                stringLimit: function(string, value){
+                    if (string.length > value){
+                        return string.substring(0,value)+ '...';
+                    }else{
+                        return string;
+                    }
+                },
+            },
+            created: function () {
+                this.getProductDetails();
             }
         });
     }
