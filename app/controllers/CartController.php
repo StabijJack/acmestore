393bc9c545afde9fc5c30612be0bec03a62a0e7e
StabijJack <jj.schneider@live.nl>
2019-01-13 16:19:16 +0100

    Creating a Cart Class php

---
+++ app/controllers/CartController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\Product;
+use App\Classes\CSRFToken;
+use App\classes\Request;
+
+class CartController extends BaseController
+{
+    public function addItem()
+    {
+        # code...
+    }
+}
--- app/routing/routes.php
+++ app/routing/routes.php
@@ -7,4 +7,7 @@ $router->map('POST','/load-more','App\Controllers\IndexController@loadMoreProduc
 
 $router->map('GET','/product/[i:id]','App\Controllers\ProductController@show','product');
 $router->map('GET','/product-details/[i:id]','App\Controllers\ProductController@get','product_details');
+
+$router->map('POST','/cart','App\Controllers\CartController@addItem','add_cart_item');
+
 require_once __DIR__.'/admin_routes.php';
