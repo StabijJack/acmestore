f588670019fd3c489cf3f2b946adf335cda83c03
StabijJack <jj.schneider@live.nl>
2019-01-16 18:06:08 +0100

    View Items in Shopping Cart - Part 3

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -40,6 +40,46 @@
                                 </tr>
                             </tbody>
                         </table>
+                        <table>
+                            <tr>
+                                <td valign="top">
+                                    <div class="input-group">
+                                        <input type="text" name="coupon" class="input-group-field" placeholder="coupon code">
+                                        <div class="input-group-button">
+                                            <button class="button">Apply</button>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <table class="unstriped">
+                                        <tr>
+                                            <td><h6>Subtotal:</h6></td>
+                                            <td class="text-right"><h6>$@{{ cartTotal }}</h6></td>
+                                        </tr>
+                                        <tr>
+                                            <td><h6>Discount Amount:</h6></td>
+                                            <td class="text-right"><h6>$0.00</h6></td>
+                                        </tr>
+                                        <tr>
+                                            <td><h6>Tax:</h6></td>
+                                            <td class="text-right"><h6>$0.00</h6></td>
+                                        </tr>
+                                        <tr>
+                                            <td><h6>Total:</h6></td>
+                                            <td class="text-right"><h6>$@{{ cartTotal }}</h6></td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="text-right">
+                            <a href="/" class="button secondary">
+                                Continue shopping &nbsp; <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                            </a>
+                            <button type="submit" class="button succes">
+                                Checkout &nbsp; <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
